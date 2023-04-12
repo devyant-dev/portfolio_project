4952fafe7e283553d6bec5bc270cf009fc793246
devyant <devvyves@gmail.com>
2023-04-12 09:46:04 +0900

    0412

--- component/Navigation.js
+++ component/Navigation.js
@@ -1,55 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import style from '@/styles/Navi.module.scss';
+import Image from 'next/image';
 
 
 const Navigation = () => {
   const router = useRouter();
+  const [act, setAct] = useState(false);
+
+  const clickMenu = () => {
+    setAct(!act)
+  }
+
+  const closeMenu = () => {
+    setAct(false)
+  }
+
 
   return (
 
-    <div className={style.menu_container}>
+    <div>
+      <div className={style.nav_container}>
+        <h1 className={style.logo}>
+          <img src="/img/profile_01.png" alt="" />
+        </h1>
 
-      
-      <div className={style.menu_wrapper}>
+        <p>
+          뭐라도적자
+        </p>
 
-        <div className={style.menu_area}>
-          <nav className={style.menu_box}>
-          
-            <button className={`${style.menu_btn} ${style.btn01}`} onClick={() => router.push('/')}>
-              <span className={style.btn_num}>01</span>
-              <span className={style.btn_title}>home</span>
-            </button>
-
-            <button className={`${style.menu_btn} ${style.btn02}`} onClick={() => router.push('/profile')}>
-              <span className={style.btn_num}>02</span>
-              <span className={style.btn_title}>profile</span>
-            </button>
-
-            <button className={`${style.menu_btn} ${style.btn03}`} onClick={() => router.push('/work')}>
-              <span className={style.btn_num}>03</span>
-              <span className={style.btn_title}>work</span>
-            </button>
-
-          </nav>
+        <div onClick={() => clickMenu()} className={`${style.nav_btn}`}>
+          <div className={`${style.nav_line} ${style.n_line01}`}></div>
+          <div className={`${style.nav_line} ${style.n_line02}`}></div>
+          <div className={`${style.nav_line} ${style.n_line03}`}></div>
         </div>
+      </div>
 
-        <div className={style.contact_area}>
-          <b className={style.contact_main_title}>Contact</b>
+      <div className={`${style.menu_container}  ${act == true ? style.act_nav : ""}`}>
 
-          <div className={style.contact_box}>
-            <p className={style.contact_title}>email</p>
-            <p className={style.contact_cont}>devvyves@gmail.com</p>
+        <div className={style.menu_wrapper}>
+
+          <div className={style.menu_area}>
+
+            <div onClick={() => closeMenu()} className={`${style.close_btn} ${act == false ? {display: "none"} : {display: "block"}}`}>
+              X
+            </div>
+
+            <nav className={style.menu_box}>
+            
+              <button className={`${style.menu_btn} ${style.btn01}`} onClick={() => router.push('/')}>
+                <span className={style.btn_num}>01</span>
+                <span className={style.btn_title}>home</span>
+              </button>
+
+              <button className={`${style.menu_btn} ${style.btn02}`} onClick={() => router.push('/profile')}>
+                <span className={style.btn_num}>02</span>
+                <span className={style.btn_title}>profile</span>
+              </button>
+
+              <button className={`${style.menu_btn} ${style.btn03}`} onClick={() => router.push('/work')}>
+                <span className={style.btn_num}>03</span>
+                <span className={style.btn_title}>work</span>
+              </button>
+
+            </nav>
           </div>
 
-          <div className={style.contact_box}>
-            <p className={style.contact_title}>mobile</p>
-            <p className={style.contact_cont}>010-2154-1107</p>
-          </div>
+          <div className={style.contact_area}>
+            <b className={style.contact_main_title}>Contact</b>
 
-          <div className={style.contact_box}>
-            <p className={style.contact_title}>GitHub</p>
-            <p className={style.contact_cont}>https://github.com/devyant-dev</p>
+            <div className={style.contact_box}>
+              <p className={style.contact_title}>email</p>
+              <p className={style.contact_cont}>devvyves@gmail.com</p>
+            </div>
+
+            <div className={style.contact_box}>
+              <p className={style.contact_title}>mobile</p>
+              <p className={style.contact_cont}>010-2154-1107</p>
+            </div>
+
+            <div className={style.contact_box}>
+              <p className={style.contact_title}>GitHub</p>
+              <p className={style.contact_cont}>https://github.com/devyant-dev</p>
+            </div>
           </div>
         </div>
       </div>
